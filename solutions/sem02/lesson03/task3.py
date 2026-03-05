69e0acfe8f767566eee903a0edfdd3eddeb7d97b
Poco-dev <vueprojector5@gmail.com>
2026-03-06 02:38:43 +0300

    lesson3

--- solutions/sem02/lesson03/task3.py
+++ solutions/sem02/lesson03/task3.py
@@ -3,4 +3,14 @@ import numpy as np
 
 def get_extremum_indices(
     ordinates: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray]:
+    if len(ordinates) < 3:
+        raise ValueError
+
+    localMin = (ordinates[1:-1] < ordinates[:-2]) & (ordinates[1:-1] < ordinates[2:])
+    min_indices = np.where(localMin)[0] + 1
+
+    localMax = (ordinates[1:-1] > ordinates[:-2]) & (ordinates[1:-1] > ordinates[2:])
+    max_indices = np.where(localMax)[0] + 1
+
+    return (min_indices, max_indices)
