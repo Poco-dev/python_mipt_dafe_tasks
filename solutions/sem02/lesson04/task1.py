f0908e61de2fe1ccc1ee90caf7b4ad3b5009ca77
Poco-dev <vueprojector5@gmail.com>
2026-03-13 23:23:19 +0300

    lesson4🫡

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,41 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError
+
+    new_shape = tuple(s + 2 * pad_size for s in image.shape[:2]) + image.shape[2:]
+    new_image = np.zeros(new_shape, dtype=image.dtype)
+    new_image[pad_size:-pad_size, pad_size:-pad_size] = image
+    return new_image
 
 
-def blur_image(
-    image: np.ndarray,
-    kernel_size: int,
-) -> np.ndarray:
-    # ваш код
-    return image
+def blur_image(image: np.ndarray, kernel_size: int) -> np.ndarray:
+    if kernel_size < 1 or kernel_size % 2 == 0:
+        raise ValueError
+
+    if kernel_size == 1:
+        return image.copy()
+
+    pad_size = kernel_size // 2
+    padded_image = pad_image(image, pad_size)
+    height, width = image.shape[:2]
+
+    prefix_sum_table = np.zeros(
+        (padded_image.shape[0] + 1, padded_image.shape[1] + 1) + image.shape[2:]
+    )
+    prefix_sum_table[1:, 1:] = padded_image.cumsum(axis=0).cumsum(axis=1)
+
+    top_left = prefix_sum_table[0:height, 0:width]
+    bottom_right = prefix_sum_table[
+        kernel_size : kernel_size + height, kernel_size : kernel_size + width
+    ]
+    top_right = prefix_sum_table[0:height, kernel_size : kernel_size + width]
+    bottom_left = prefix_sum_table[kernel_size : kernel_size + height, 0:width]
+
+    window_sum = bottom_right + top_left - top_right - bottom_left
+    blurred_image = (window_sum / (kernel_size * kernel_size)).astype(image.dtype)
+    return blurred_image
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,6 +5,29 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
 
-    return 0, 0
+    arr = np.array(image).ravel()
+    if arr.size == 0:
+        return np.uint8(0), 0.0
+
+    uniq, counts = np.unique(arr, return_counts=True)
+    color_freq = np.zeros(256)
+    color_freq[uniq] = counts
+
+    prefix_sum = np.zeros(257)
+    prefix_sum[1:] = np.cumsum(color_freq)
+
+    index_array = np.arange(256)
+    left_indexes = np.clip(index_array - threshold + 1, 0, 255)
+    right_indexes = np.clip(index_array + threshold - 1, 0, 255)
+
+    window_sums = prefix_sum[right_indexes + 1] - prefix_sum[left_indexes]
+    window_sums[color_freq == 0] = -1
+
+    best_color = np.argmax(window_sums)
+    best_sum = window_sums[best_color]
+    percent = best_sum / arr.size * 100
+
+    return np.uint8(best_color), percent
